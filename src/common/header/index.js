450d171c2e18277f5369c1685eb31fc3230efa72
Liepin-NorthAmerica <nini10171017>
2019-04-20 12:57:29 -0400

    finished 7_3

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,30 @@
 import React , { Component } from 'react';
-import { HeaderWrapper, Logo, Nav } from './style';
+import { 
+    HeaderWrapper, 
+    Logo, 
+    Nav, 
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
+} from './style';
 
 class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
                <Logo />
-               <Nav />
+               <Nav>
+                   <NavItem className="left active">Home</NavItem>
+                   <NavItem className="left">Download App</NavItem>
+                   <NavItem className="right">Log In</NavItem>
+                   <NavItem className="right">Aa</NavItem>
+                   <NavSearch></NavSearch>
+                   <Addition>
+                       <Button className="writting">Compose</Button>
+                       <Button className="reg">Register</Button>
+                   </Addition>
+               </Nav>
             </HeaderWrapper>
         )
     }
