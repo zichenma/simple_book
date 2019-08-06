1ace63a8c8ebb0985e6b77549a87b5f0401d5eb6
Liepin-NorthAmerica <nini10171017>
2019-08-06 08:24:34 -0400

    finsihed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import Header from './common/header';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      // Provider 只能有一个child
       <Provider store={store}>
+       <div>
         <Header />
+        {/* BrowserRouter 只能有一个child */}
+        <BrowserRouter>
+          <div>
+            {/* exact 标明只有路径完全相符的情况下才匹配 */}
+            <Route path='/' exact render={() => <div>home</div> }></Route>
+            <Route path='/detail' exact render={() => <div>detail</div> }></Route>
+          </div>
+        </BrowserRouter>
+       </div>
       </Provider>
     );
   }
