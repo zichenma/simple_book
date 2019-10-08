8756291122536482c35057c159730979680e07aa
Liepin-NorthAmerica <nini10171017>
2019-10-07 23:56:41 -0400

    Finsihed 8_2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
 import { HomeWrapper, HomeLeft, HomeRight } from './style';
 
 class Home extends Component {
     render() {
         return (
             <HomeWrapper>
-                <HomeLeft>Left</HomeLeft>
-                <HomeRight>Right</HomeRight>
+                <HomeLeft>
+                    <img className='banner-img' src="//upload.jianshu.io/admin_banners/web_images/4732/5d0b947b56c13a3d5e96fe8fe77c66079deed975.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" />
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
             </HomeWrapper>
         )
     }
