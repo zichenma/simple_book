29a4d45930f099b04ff74dbcc5955aa1ab84c8fe
Liepin-NorthAmerica <nini10171017>
2019-10-26 19:53:34 -0400

    finished 8_5

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -36,6 +36,12 @@ const defaultState = fromJS({
             desc: 'It is quite a task thinking up great made-up words that are unique, so I created this word generator to help you come up with the best fake word ideas. They can be great for naming your website, business, product or project. Fake words or pseudowords are words which look like they are real, but actually have no meaning.',
             imgUrl: 'https://upload-images.jianshu.io/upload_images/14386767-150a1ede549eb290.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
         }
+    ],
+    recommendList: [
+        {id : 1, imgUrl : 'http://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png'},
+        {id : 2, imgUrl : 'http://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'},
+        {id : 3, imgUrl : 'http://cdn2.jianshu.io/assets/web/banner-s-5-4ba25cf5041931a0ed2062828b4064cb.png'},
+        {id : 4, imgUrl : 'http://cdn2.jianshu.io/assets/web/banner-s-6-c4d6335bfd688f2ca1115b42b04c28a7.png'},
     ]
 });
 
