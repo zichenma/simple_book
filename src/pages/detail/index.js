c44584e4cccb6518829fbfe51b629326e46db248
Zichen Ma <[mazichen1984@gmail.com]>
2019-11-18 10:38:41 -0500

    finsihed

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component {
     render() {
         return (
-            <div>Detail~</div>
+            <DetailWrapper>
+                <Header>
+                    Heer dafe dasf fe5f223
+                </Header>
+                <Content>
+                    <img src="//upload-images.jianshu.io/upload_images/15872426-587e3bbc0967b1de.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/386/format/webp" />
+                    <p>TrendingHand DrawingFake Word Generator</p>
+                    <p><b>It is quite a task thinking up great made-up words that are unique</b></p>
+                    <p>so I created this word generator to help you come up with the best fake word ideas</p>
+                    <p>They can be great for naming your website, business, product or project. Fake words or pseudowords are words which look like they are real, but actually have no meaning.</p>
+                </Content>
+            </DetailWrapper>
         )
     }
 }
